ae670001c7c91a9e84380ef4e0ee3d71578be21b
Chandrasekharsamanta <chandrasekhar.jagannath@eminds.ai>
2024-04-21 21:59:43 +0530

    init ionic angular

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'media-capture',
+  webDir: 'www'
+};
+
+export default config;
